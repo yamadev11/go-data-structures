7620d9d2f34a825ab000dbb427ee3c277e402534
yamadev11 <ymaheshwari99@gmail.com>
2023-05-08 13:19:13 +0530

    added swap method in linkedlist

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -9,27 +9,32 @@ type node struct {
 }
 
 type LinkedList struct {
-	head *node
+	head   *node
+	length int
 }
 
 func NewLinkedList(values ...interface{}) *LinkedList {
 	var head, curr *node
+	var length int
 	for i, value := range values {
 		if i == 0 {
 			head = &node{val: value, next: nil}
 			curr = head
+			length = 1
 			continue
 		}
 		curr.next = &node{val: value, next: nil}
 		curr = curr.next
+		length++
 	}
 
-	return &LinkedList{head: head}
+	return &LinkedList{head: head, length: length}
 }
 
 func (list *LinkedList) AddNode(val interface{}) {
 	if list.head == nil {
 		list.head = &node{val: val, next: nil}
+		list.length = 1
 		return
 	}
 
@@ -39,6 +44,7 @@ func (list *LinkedList) AddNode(val interface{}) {
 	}
 
 	curr.next = &node{val: val, next: nil}
+	list.length += 1
 }
 
 func (list *LinkedList) PrintList() {
@@ -78,6 +84,7 @@ func (list *LinkedList) DeleteNode(val interface{}) {
 	if list.head.val == val {
 		fmt.Println("Deleting Node having value", val)
 		list.head = list.head.next
+		return
 	}
 
 	curr := list.head
@@ -85,9 +92,45 @@ func (list *LinkedList) DeleteNode(val interface{}) {
 		if curr.next.val == val {
 			fmt.Println("Deleting Node having value", val)
 			curr.next = curr.next.next
+			return
 		}
 
 		curr = curr.next
 	}
 
+}
+
+func (list *LinkedList) SwapNode(index1, index2 int) {
+	if index1 > list.length || index2 > list.length || index1 < 0 || index2 < 0 {
+		// log error
+		return
+	}
+
+	var node1Prev, node2Prev, node1Next, node2Next *node
+	node1 := list.head
+	for i := 1; i <= index1; i++ {
+		node1Prev = node1
+		node1 = node1.next
+		node1Next = node1.next
+	}
+
+	node2 := list.head
+	for i := 1; i <= index2; i++ {
+		node2Prev = node2
+		node2 = node2.next
+		node2Next = node2.next
+	}
+
+	node2Prev.next = node1
+	node1.next = node2Next
+	node1Prev.next = node2
+	node2.next = node1Next
+}
+
+func (list *LinkedList) Length() int {
+	if list != nil {
+		return list.length
+	}
+
+	return 0
 }
